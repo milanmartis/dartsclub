95fb58313a3345b07be423216e606cae9a62f761
unknown <milanmartis@gmail.com>
2023-02-17 09:06:12 +0100

    darts-V2023-02-17a

--- website/__init__.py
+++ website/__init__.py
@@ -21,7 +21,7 @@ DB_NAME = "../instance/"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("my_secret_key")
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("sql_url")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ynqryzyuztgqts:122f26414b20598848fc10a2703fd6da06650c06918c1a69e5e7249d59597271@ec2-34-194-40-194.compute-1.amazonaws.com:5432/d8jkicn6gvjnuh'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(DB_NAME, 'database.db')
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -37,15 +37,17 @@ def create_app():
     from .models import User, Note, Groupz, Duel
 
     app.app_context().push()
-    # db.create_all()
+    db.create_all()
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(id):
-        return User.query.filter_by(email=id).first()
+    def load_user(email):
+        print(email)
+        return User.query.filter_by(email=email).first()
+        
 
 
    
