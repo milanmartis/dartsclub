9bfbd0e9ef89022ac5af66ee2af0ea55a07390c7
unknown <milanmartis@gmail.com>
2023-02-26 11:45:26 +0100

    dartsc v -2023-02-26-d

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --handled
+web: gunicorn app:app --hand
--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def create_tables():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=int(os.environ.get("PORT", 5000)),host='127.0.0.1',debug=True)
