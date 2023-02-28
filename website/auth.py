eb8d72882896ee358b31a23acb64790827d5fa2c
unknown <milanmartis@gmail.com>
2023-02-28 09:50:30 +0100

    dartsc v -2023-02-28-e

--- website/auth.py
+++ website/auth.py
@@ -49,7 +49,9 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+
         user = User.query.filter_by(email=email).first()
+        db.session.rollback()
 
         if check_password_hash(user.password, password):
             # session["user_email"] = user.email
