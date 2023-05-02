93942c23a7ee9f61600756d9b3086dba52cc3bcd
unknown <milanmartis@gmail.com>
2023-05-02 23:24:46 +0200

    2023-05-02-12

--- website/auth.py
+++ website/auth.py
@@ -220,8 +220,8 @@ def send_reset_email(user):
 <p>If you did not make this request then simply ignore this email and no changes will be made.</p>
 <br>
 <br>
-<img width="190" src="{ current_app.url_for('static', filename='img/logo-darts-mail.png', _external=True) }">
-
+  <source srcset="{ current_app.url_for('static', filename='img/logo-dark.png', _external=True) }" media="(prefers-color-scheme: dark)">
+  <img width="120" src="{ current_app.url_for('static', filename='img/logo-light.png', _external=True) }">
 <h5>©4NOLIMIT. POWERED BY APPDESIGN.SK</h5>
 </center>
 '''
@@ -233,9 +233,32 @@ def send_confirm_email(user):
     msg = Message('Confirm your register email',
                   sender=('Darts Club', 'info@dartsclub.sk'),
                   recipients=[user.email])
-    msg.body = f'''To confirm your email, click on the following link:
-{url_for('auth.confirm_token', token=token, _external=True)}
-If you did not make this request then simply ignore this email.
+    msg.body = f'''<center><h1>To confirm your email, click on the following link</h1>
+<br>
+<br>
+<a style="
+    border-radius: 14px !important;
+    background-color: #00EE00;
+    border: 2px solid #00EE00;
+    color: #030303;
+    font-weight: 610;
+    font-size: 120%;
+    text-decoration:none;
+    cursor:pointer;
+    padding: 8px 12px 8px 12px;
+    margin: 12px;
+    width:300px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;" 
+    href="{url_for('auth.confirm_token', token=token, _external=True)}">CONFIRM ACCOUNT</a>
+<br>
+<br>
+<p>If you did not make this request then simply ignore this email and no changes will be made.</p>
+<br>
+<br>
+  <source srcset="{ current_app.url_for('static', filename='img/logo-dark.png', _external=True) }" media="(prefers-color-scheme: dark)">
+  <img width="120" src="{ current_app.url_for('static', filename='img/logo-light.png', _external=True) }">
+<h5>©4NOLIMIT. POWERED BY APPDESIGN.SK</h5>
+</center>
 '''
     mail.send(msg)
 
